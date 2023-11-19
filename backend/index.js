6faccd68932bb70b873f486fa1b69cddf09c4d06
Phone Aung <phonexaung@gmail.com>
2023-11-19 23:07:39 +0630

    create route for save a new book

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Welcome to Bookstore!')
+});
+
+// Route for save a new book
+app.post('/books', async (request, response) => {
+    try {
+        // Validate input 
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear'
+            });
+        }
+        
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
 
 mongoose.connect(mongoDBURL)
