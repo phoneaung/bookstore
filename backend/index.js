84aabd2df05a5bfd85421779f2210a5f10da0cb7
Phone Aung <phonexaung@gmail.com>
2023-11-19 22:32:19 +0630

    fix typo config

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { PORT } from "./config";
+import { PORT } from "./config.js";
 
 const app = express();
 
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
 });
+
