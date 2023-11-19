a3a9ed3b85e1802fde6d352be5f757a32d3b3887
Phone Aung <phonexaung@gmail.com>
2023-11-19 22:49:01 +0630

    add mongodb and mongoose to nodejs

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@bookstore.4dlqmb1.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
 
 const app = express();
 
@@ -8,7 +8,14 @@ app.get('/', (request, response) => {
     return response.status(234).send('Welcome to Bookstore!')
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-});
+mongoose.connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database.');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
