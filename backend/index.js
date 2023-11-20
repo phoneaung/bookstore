87d2775da2aefdbafe5c00aaa8817c9c2ae2718d
Phone Aung <phonexaung@gmail.com>
2023-11-20 20:30:03 +0630

    add middleware for parsing request body - postman

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+// Middleware for parsing request body
+app.use(express.json());
+
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Welcome to Bookstore!')
