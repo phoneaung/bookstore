eb25a7893dbacf5b4ef60e257f648d978bf8ce1f
Phone Aung <phonexaung@gmail.com>
2023-11-20 21:02:28 +0630

    fix typo error

--- backend/index.js
+++ backend/index.js
@@ -113,7 +113,7 @@ app.delete('/books/:id', async (request, response) => {
     try {
         const { id } = request.params;
 
-        const result = await Book.findFindByIdAndDelete(id);
+        const result = await Book.findByIdAndDelete(id);
 
         if(!result) {
             return response.status(404).json({ message: 'Book not found!' });
