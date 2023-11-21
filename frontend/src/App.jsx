ba7e7e6cfde9bc8ab6c883fd6f2cab363ec1d77a
Phone Aung <phonexaung@gmail.com>
2023-11-21 22:27:58 +0630

    import each pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import ShowBooks from './pages/ShowBooks';
+import EditBook from './pages/EditBook';
+import DeleteBooks from './pages/DeleteBooks';
+
+
+
 
 export const App = () => {
   return (
